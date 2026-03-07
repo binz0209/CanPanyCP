c360c1c1336b15b20febada60aa46ce1a2bf06aa
anthien <thientdm2004@gmail.com>
2026-03-07 13:25:22 +0700

    Add Api for Jobs and application (Candidates))

--- CanPany-FE/src/api/index.ts
+++ CanPany-FE/src/api/index.ts
@@ -4,3 +4,4 @@ export { jobsApi } from './jobs.api';
 export { companiesApi } from './companies.api';
 export { candidateApi } from './candidate.api';
 export { cvApi } from './cv.api';
+export { applicationsApi } from './applications.api';
